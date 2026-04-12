58e7e1ea28e8aa2d471e776e0e464c07931d2085
Jack Sangl <sangljack06@gmail.com>
2026-04-12 15:07:34 -0400

    Add: longest-common-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector <vector<int>> dp (text1.size()+1, vector<int>(text2.size()+1, 0));
+        int m = text1.size(), n = text2.size();
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1[i-1] == text2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        return dp[m][n];
+    }
+};
